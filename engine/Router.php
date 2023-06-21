05a736c27e07d74a9c22729d1935275c77bc9891
mayowa1305 <69318084+mayowa1305@users.noreply.github.com>
2023-06-21 17:09:28 +0100

    Create Router.php

---
+++ engine/Router.php
@@ -0,0 +1,29 @@
+<?php
+namespace php_blog\engine;
+
+class Router{
+
+    public static function run(array $aParams){
+        $sNamespace = 'php_blog\controller\\';
+        $sDefCtrl = $sNamespace.'blog';
+        $sCtrlPath = ROOT_PATH.'controller/';
+        $sCtrl = ucfirst($aParams['ctrl']) ;
+
+        if (is_file(($sCtrlPath.$sCtrl.'.php'))){
+          $sCtrl = $sNamespace.$sCtrl;
+          $oCtrl = new $sCtrl;
+          
+          if ((new \ReflectionClass($oCtrl))->hasMethod($aParams['act']) && (new \ReflectionMethod($oCtrl, $aParams['act']))->isPublic()){
+            call_user_func(array($oCtrl,$aParams['act']));
+        } else{
+            call_user_func(array($oCtrl,'notfound'));
+        }
+
+
+        }else{
+        call_user_func(array($sDefCtrl,'notfound'));
+        }
+    
+    }
+}
+?>
