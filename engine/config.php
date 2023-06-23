46b13c272ba26835e124fce852a2cf642df94965
mayowa1305 <adeyinka.boluwatife17@gmail.com>
2023-06-23 09:47:34 +0100

    edited db config

--- engine/config.php
+++ engine/config.php
@@ -4,7 +4,7 @@ namespace php_blog\engine;
 
 final class config{
     const
-    DB_HOST = 'localhost',
+    DB_HOST = '127.0.0.1',
     DB_NAME = 'mymvcblog',
     DB_USER = 'root',
     DB_PASSWORD = '',
