da7383e9430cfa3917989b211b323b25deb6ce4b
mayowa1305 <adeyinka.boluwatife17@gmail.com>
2023-06-30 04:57:23 +0100

    edited config

--- engine/config.php
+++ engine/config.php
@@ -4,11 +4,11 @@ namespace php_blog\engine;
 
 final class config {
     const
-    DB_HOST = '127.0.0.1',
-    DB_PORT = '3306',
-    DB_NAME = 'mymvcblog',
-    DB_USER = 'blogdb',
-    DB_PASSWORD = 'zTN(j9EP41eyuLd[',
+    DB_HOST = 'containers-us-west-195.railway.app',
+    DB_PORT = '6788',
+    DB_NAME = 'railway',
+    DB_USER = 'root',
+    DB_PASSWORD = 'UEaLVhLd0ravtP1UTUgP',
     
     SITE_NAME = 'my blog';
 }
